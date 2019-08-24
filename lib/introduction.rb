5a4b3fc73181ca86a5628f85d1e0b297e1ac9f0b
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-08-24 01:18:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,10 +9,10 @@ def introduction(name) do
 end
 
 def introduction_with_language(name,language) do
-  puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name} and I am learning to program in #{name}."
 end
 
-
+def introduction_with_language_optional(name, language = "Ruby")
 
 
 
